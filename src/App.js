8b9d2f59c7048af6661f77b1e1a5cddd3ce1f52f
mikenic <temperanceaubie@gmail.com>
2018-07-06 20:56:29 -0700

    first setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class App extends React.Component{
+	render() {
+		return(
+			<div className="container text-center">
+				<h1>Todo App</h1>
+			</div>
+		
+		);
+	}
+}
+module.exports = App;
